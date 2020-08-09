06d60d03c86c90b5fd87d94888c3ea9a7a558fc7
iamarjun <arjunmanoj1995@gmail.com>
2020-08-09 19:11:23 +0530

    WIP setup di and setup network module

---
+++ app/src/main/java/com/arjun/recipe/Constants.kt
@@ -0,0 +1,7 @@
+package com.arjun.recipe
+
+object Constants {
+
+    const val BASE_URL = "https://recipesapi.herokuapp.com/api/"
+
+}
---
+++ app/src/main/java/com/arjun/recipe/RestApi.kt
@@ -0,0 +1,5 @@
+package com.arjun.recipe
+
+interface RestApi {
+
+}
