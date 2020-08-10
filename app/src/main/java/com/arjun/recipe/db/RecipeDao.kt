fa19635b4bf08cdae3b737a8b354bb00c6fc8df6
iamarjun <arjunmanoj1995@gmail.com>
2020-08-10 19:25:40 +0530

    write the db queries

--- app/src/main/java/com/arjun/recipe/db/RecipeDao.kt
+++ app/src/main/java/com/arjun/recipe/db/RecipeDao.kt
@@ -1,7 +1,21 @@
 package com.arjun.recipe.db
 
-import androidx.room.Dao
+import androidx.room.*
+import com.arjun.recipe.model.Recipe
 
 @Dao
 interface RecipeDao {
+
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    suspend fun insertRecipeList(list: List<Recipe>)
+
+    @Update
+    suspend fun updateRecipe(recipe: Recipe)
+
+    @Query("select * from recipe where recipeId like '%'||:recipeId||'%'")
+    suspend fun getRecipe(recipeId: String): Recipe
+
+    @Query("select * from recipe where lower(title) like '%'||:query||'%' order by socialRank desc")
+    fun getRecipeList(query: String): List<Recipe>
+
 }
