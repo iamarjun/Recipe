ac8b44afb1cf4f32f21abed1ee7997b4708a1e93
iamarjun <arjunmanoj1995@gmail.com>
2020-08-10 22:32:46 +0530

    remove unwanted methods

--- app/src/main/java/com/arjun/recipe/repositories/RecipeRepository.kt
+++ app/src/main/java/com/arjun/recipe/repositories/RecipeRepository.kt
@@ -6,25 +6,12 @@ import com.arjun.recipe.db.RecipeDatabase
 import com.arjun.recipe.model.Recipe
 import com.arjun.recipe.networkBoundResource
 import kotlinx.coroutines.flow.Flow
-import timber.log.Timber
 import javax.inject.Inject
 
 class RecipeRepository @Inject constructor(
     private val restApi: RestApi,
     private val db: RecipeDatabase
 ) {
-
-    suspend fun recipeList(query: String): List<Recipe> {
-        Timber.d("New query: $query")
-        val response = restApi.searchRecipe(query)
-        return response.recipes
-    }
-
-    suspend fun recipeDetail(recipeId: String): Recipe {
-        Timber.d("Recipe Id: $recipeId")
-        val response = restApi.getRecipe(recipeId)
-        return response.recipe
-    }
 
     fun getRecipeList(): Flow<Resource<List<Recipe>>> = networkBoundResource(
         query = { db.recipeDao.getRecipeListFlow() },
